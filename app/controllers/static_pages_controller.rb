daecc7757187b56ee52a47bd6107cde9638a213b
Fred <fanfanshao@gmail.com>
2016-06-17 16:24:25 +0800

    Add Get Ip Test Code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+  require "net/http"
 
   def home
     @timestamp = Time.now.to_i.to_s
+    @client_ip = request.remote_ip
+
+    url_get_city = URI("http://ip.taobao.com/service/getIpInfo.php?ip=180.173.171.92")
+    @res = Net::HTTP.get_response(url_get_city).body
   end
 
 
